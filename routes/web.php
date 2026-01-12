06825009e2d4b813e02eb6605eed99b3306d7f0a
Sourouya-Abdoulaye <akisourouyaabdoulaye@gmail.com>
2026-01-12 13:39:15 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+use App\Controllers\Usercontroller;
+
+$router = new AltoRouter();
+//pour vider (ca nous permet d'eviter des erreurs innatandit)
+$router->setBasePath('');
+
+
+// creation des routes
+$router->map('GET', '/', function() { echo "Welcome Page"; });
+
+
+
+// je vais matcher pour voi les correspondante
+$match = $router->match();
+//print_r($match);
+
+
+if($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+   
+}
+
+
